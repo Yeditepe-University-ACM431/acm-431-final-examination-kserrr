c2a69a70aad6e6f2f95ddd37c7391b96b53de566
kserrr <deniz.aktosun@std.yeditepe.edu.tr>
2026-01-06 15:15:47 +0300

    Update TaskViewModel.kt

--- app/src/main/java/com/yeditepe/finalexam/viewmodel/TaskViewModel.kt
+++ app/src/main/java/com/yeditepe/finalexam/viewmodel/TaskViewModel.kt
@@ -6,8 +6,23 @@ class TaskViewModel : ViewModel() {
 
     // TODO 1: Create a mutable state list of Task objects
     // Initially add at least 2 tasks
+private val_tasks = mutableStateOff(
+    listOf( Task(id = 1, title = "Submit Final Project", isCompleted = false),
+    Task(id = 2, title = "Study for Exam", isCompleted = true)
+    )
+    )
+    val tasks: State<List<Task>> = _tasks
 
     fun toggleTask(taskId: Int) {
         // TODO 2: Update isCompleted for the given task
+        fun toggleTask(taskId: Int) { _tasks.value =  _tasks.value.map { task ->
+                if (task.id == taskId) {
+           task.copy(isCompleted = !task.isCompleted)
+                } else {
+                    task
+                }
+            }
+        }
+
     }
 }
