351f1787e4ae181e111984cbc0fcf6ae8db8e82d
kserrr <deniz.aktosun@std.yeditepe.edu.tr>
2026-01-06 15:20:39 +0300

    Update TaskRepository.kt

--- app/src/main/java/com/yeditepe/finalexam/repository/TaskRepository.kt
+++ app/src/main/java/com/yeditepe/finalexam/repository/TaskRepository.kt
@@ -2,12 +2,23 @@ package com.yeditepe.finalexam.repository
 
 import com.yeditepe.finalexam.api.TaskApi
 import com.yeditepe.finalexam.model.Task
+import com.yeditepe.finalexam.model.TaskDto
+
 
 class TaskRepository(private val api: TaskApi) {
 
     suspend fun fetchTasks(): List<Task> {
-        // TODO 1: Call API
-        // TODO 2: Convert TaskDto list to Task list
+            val taskDtos = api.getTasks()
+
+            return taskDtos.map { dto ->
+                Task(
+                    id = dto.id,
+                    title = dto.title,
+                    isCompleted = dto.completed
+                )
+            }
+        }
+
         return TODO("Provide the return value")
     }
 }
